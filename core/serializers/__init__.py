3c2e45a9968579e17d9a30a31c076237e22e14fa
thaynabecker <thaynabecker.ifcaraquari@gmail.com>
2026-04-10 10:52:54 -0300

    feat: criação de dois serializadores para Livro

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,4 +2,4 @@ from .user import UserRegistrationSerializer, UserSerializer
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
-from .livro import LivroSerializer
+from .livro import LivroListRetrieveSerializer, LivroSerializer
