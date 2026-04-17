53273e045755cf9f14a68b79ae697bbb60995b41
thaynabecker <thaynabecker.ifcaraquari@gmail.com>
2026-04-17 10:57:13 -0300

    feat: inclusão da app de upload e associação de imagens

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django_filters',
     'drf_spectacular',
     'rest_framework',
+    'uploader',
     'core',
 ]
 
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,12 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from core.models import Livro
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 
-class LivroSerializer(ModelSerializer):
+class LivroRetrieveSerializer(ModelSerializer):
+    capa = ImageSerializer(required=False)
+
     class Meta:
         model = Livro
         fields = '__all__'
+        depth = 1
 
 
 class LivroListSerializer(ModelSerializer):
@@ -15,8 +20,16 @@ class LivroListSerializer(ModelSerializer):
         fields = ('id', 'titulo', 'preco')
 
 
-class LivroRetrieveSerializer(ModelSerializer):
+class LivroSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source='capa',
+        queryset=Image.objects.all(),
+        slug_field='attachment_key',
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Livro
         fields = '__all__'
-        depth = 1
