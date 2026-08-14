8192f8c8588e2cad81d005f85faa454db96ee9b7
thaynabecker <thaynabecker.ifcaraquari@gmail.com>
2026-08-14 08:56:36 -0300

    feat: inclusão da foto de perfil no usuário

--- core/models/user.py
+++ core/models/user.py
@@ -10,6 +10,8 @@ from django.contrib.auth.models import (
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from uploader.models import Image
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -19,9 +21,12 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
-            raise ValueError('Users must have an email address.')
+            raise ValueError("Users must have an email address.")
 
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields,
+        )
         user.set_password(password)
         user.save(using=self._db)
 
@@ -40,24 +45,45 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     """User model in the system."""
 
-    email = models.EmailField(max_length=255, unique=True, verbose_name=_('email'), help_text=_('Email'))
-    name = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('name'), help_text=_('Username'))
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+        verbose_name=_("email"),
+        help_text=_("Email"),
+    )
+    name = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name=_("name"),
+        help_text=_("Username"),
+    )
     is_active = models.BooleanField(
-        default=True, verbose_name=_('Usuário está ativo'), help_text=_('Indica que este usuário está ativo.')
+        default=True,
+        verbose_name=_("Usuário está ativo"),
+        help_text=_("Indica que este usuário está ativo."),
     )
     is_staff = models.BooleanField(
         default=False,
-        verbose_name=_('Usuário é da equipe'),
-        help_text=_('Indica que este usuário pode acessar o Admin.'),
+        verbose_name=_("Usuário é da equipe"),
+        help_text=_("Indica que este usuário pode acessar o Admin."),
+    )
+    foto = models.ForeignKey(
+        Image,
+        related_name="user_foto",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
     )
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     class Meta:
         """Meta options for the model."""
 
-        verbose_name = 'Usuário'
-        verbose_name_plural = 'Usuários'
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
